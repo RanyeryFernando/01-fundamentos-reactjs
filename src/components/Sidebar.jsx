04ab34c3fbe54d9d2b0eae953da87bc67866b39a
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2022-08-26 21:19:34 -0300

    Finalizando a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
  
 export function Sidebar() {
@@ -10,12 +12,14 @@ export function Sidebar() {
             />
             
         <div className={styles.profile}>
-            <strong>Diego Fernandes</strong>
+            <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/83469345?v=4" alt="" />
+            <strong>Ranyery Fernando</strong>
             <span>Web Developer</span>
         </div>
 
         <footer>
             <a href="#">
+                <PencilLine size={18} />
                 Editar seu perfil
             </a>
 
